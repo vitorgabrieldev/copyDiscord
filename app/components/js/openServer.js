0a0e2fbeb4d674210f2a7ef2ad44fffed9406e4c
vitorgabrielgit <vitorgabrieldeoliveiragit@gmail.com>
2022-09-03 19:01:10 -0300

    teste

--- app/components/js/openServer.js
+++ app/components/js/openServer.js
@@ -1,9 +1,11 @@
 let openCanais1 = document.querySelector('#openServidor').addEventListener('click', () =>
-{
-    this.openServer.openServidor();
-});
+{this.openServer.openServidor();});
+
+let openFront = document.querySelector('#openFront').addEventListener('click', () =>
+{this.openServer.openFrontEnd();});
 
 var openServer = {
+
     openServidor: function()
     {
         let channelsServidor = document.querySelector('#channelsServidor');
@@ -13,5 +15,17 @@ var openServer = {
          } else {
             channelsServidor.style.display = 'none';
          };
+    },
+
+    openFrontEnd: function()
+    {
+        let channelsFront = document.querySelector('#channelsFront');
+
+        if(channelsFront.style.display === 'none')
+        {
+           channelsFront.style.display = 'block';
+        } else {
+           channelsFront.style.display = 'none';
+        };
     },
 };
