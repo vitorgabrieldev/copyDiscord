4980b0a0a01c338c998fb36a8017b4f8bfacf39f
vitorgabrielgit <vitorgabrieldeoliveiragit@gmail.com>
2022-09-05 17:33:12 -0300

    chat

--- app/components/js/sendMsg.js
+++ app/components/js/sendMsg.js
@@ -4,6 +4,32 @@ window.addEventListener('keypress', (e) =>
 {
     if(e.key === 'Enter')
     {
-        alert('Hello, World!')
+        let chat = document.querySelector('#chat').value;
+
+        if(chat)
+        {
+            this.chat.main();
+        };
     };
 });
+
+var chat = {
+
+    main: function()
+    {
+        this.createMsg();
+    },
+
+    createMsg: function()
+    {
+
+        let containerMsg = document.querySelector('#containerMsg');
+        let chat = document.querySelector('#chat').value;
+
+        var listMsg = document.createElement("li");
+        listMsg.textContent = "VitorK > " + chat;
+        containerMsg.appendChild(listMsg);
+
+    },
+
+};
